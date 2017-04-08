2ca2eea9430427ad732557a027d84fa23eea2e2b
xu kangfeng <xu kangfeng>
2017-04-08 11:24:58 +0800

    Initial commit

---
+++ src/main/java/com/tiago/SpringBootTiagoPracticeApplication.java
@@ -0,0 +1,12 @@
+package com.tiago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTiagoPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTiagoPracticeApplication.class, args);
+	}
+}
